dac0250ffebee3e95a3d5519daf51e0e99b01cb2
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-01-09 00:22:14 +0200

    update (works without load more button)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './styles.css';
+import imageCard from './templates/image-card.hbs';
+import ImagesApiService from './js/apiService';
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    imagesContainer: document.querySelector('.gallery'),
+}
+
+const imagesApiService = new ImagesApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    imagesApiService.query = e.currentTarget.elements.query.value;
+
+    if (imagesApiService.query === '') {
+        return alert('Type something');
+    }
+
+    imagesApiService.resetPage();
+    clearImagesContainer();
+    fetchImages();
+}
+
+function fetchImages() {
+    imagesApiService.fetchImages().then(hits => {
+        appendImagesMarkup(hits);
+    });
+}
+
+function appendImagesMarkup(hits) {
+    refs.imagesContainer.insertAdjacentHTML('beforeend', imageCard(hits));
+}
+
+function clearImagesContainer() {
+    refs.imagesContainer.innerHTML = '';
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,38 @@
+const API_KEY = '19813878-66abafb147b791a25f56ab6fb';
+const BASE_URL = 'https://pixabay.com/api';
+// const options = { headers: { Authorization: API_KEY, }, };
+
+export default class ImagesApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    
+    fetchImages() {
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+        return fetch(url)
+            .then(response => response.json())
+            .then(({ hits }) => {
+                this.incrementPage();
+                return hits;
+            });
+    }
+    
+    incrementPage() {
+            this.page += 1;
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        return this.searchQuery = newQuery;
+    }
+}
+
+// const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ';
