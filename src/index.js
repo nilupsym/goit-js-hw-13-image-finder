e0a617bcc60eea8db777ca498d8c1975e234df09
Andriy Rudkovsky <a.rudkovskyy@gmail.com>
2021-01-09 15:41:17 +0200

    adds load more button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import './styles.css';
 import imageCard from './templates/image-card.hbs';
 import ImagesApiService from './js/apiService';
+import LoadMoreButton from './js/components/load-more-button';
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
     imagesContainer: document.querySelector('.gallery'),
 }
 
+const loadMoreButton = new LoadMoreButton({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
+
 const imagesApiService = new ImagesApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
+loadMoreButton.refs.button.addEventListener('click', fetchImages);
 
 function onSearch(e) {
     e.preventDefault();
@@ -20,14 +27,17 @@ function onSearch(e) {
         return alert('Type something');
     }
 
+    loadMoreButton.show();
     imagesApiService.resetPage();
     clearImagesContainer();
     fetchImages();
 }
 
 function fetchImages() {
+    loadMoreButton.disable();
     imagesApiService.fetchImages().then(hits => {
         appendImagesMarkup(hits);
+        loadMoreButton.enable();
     });
 }
 
